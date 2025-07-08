f8a9d7fedca7e2d1f68b73a606fe0169ab4f699d
Ryeid Hanif <ryeidhanif@gmail.com>
2025-07-08 12:50:40 +0500

    feat:added git action

--- main/views.py
+++ main/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
     return render(request , "main/home.html")
 
 
-
+@login_required(login_url="/login/")
 def dashboard(request):
     return render(request, "main/dashboard.html")
 
