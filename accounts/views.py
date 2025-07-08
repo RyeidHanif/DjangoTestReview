d70fbb8381e4be193ee08d49b857fa975d1b3217
Ryeid Hanif <ryeidhanif@gmail.com>
2025-07-08 10:15:21 +0500

    feat: projectsetup + auth

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
+# Create your views here.
+
+
+
+def signup(request):
+
+    if request.method == "POST":
+        suform = SignUpForm(request.POST)
+        if suform.is_valid():
+            suform.save()
+            return redirect("login")
+    
+    suform = SignUpForm()
+    return render(request , "accounts/signup.html", {"form": suform})
+
+    
---
+++ main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request , "main/home.html")
+
+
+
+def dashboard(request):
+    return render(request, "main/dashboard.html")
+
+
