5b6c973cccd4b59a9126d2d1d87fefc1eeac4a1c
Ryeid Hanif <ryeidhanif@gmail.com>
2025-07-09 10:43:51 +0500

    feat:changed user class + gitignore

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
-# from django.db import models
-
+from django.db import models
+from  django.contrib.auth.models import AbstractUser
+import uuid
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phoneno = models.CharField(max_length=13)
+    email = models.EmailField(unique = True)
+
+    USER_TYPE_OPTIOSN = (
+        ('customer', 'Customer'),
+        ('provider', 'Provider')
+    )
+ 
+    user_type = models.CharField(max_length=10 , choices = USER_TYPE_OPTIOSN, default ="customer")
+
+    REQUIRED_FIELDS =[ "email", "user_type", "phoneno",]
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+     "accounts",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -45,7 +46,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main",
-    "accounts",
     "crispy_forms",
     "crispy_bootstrap5",
 ]
@@ -84,9 +84,13 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'appointmentdb',
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -137,3 +141,6 @@ LOGIN_REDIRECT_URL = "/dashboard/"
 LOGOUT_REDIRECT_URL = "/"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
