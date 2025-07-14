e258aa5d86ee8aa144349dba88c68f4728d73b5d
Ryeid Hanif <ryeidhanif@gmail.com>
2025-07-14 14:27:40 +0500

    feat : password reset systme added

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,38 @@
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from .forms import SetPasswordForm
+from django.contrib.auth.decorators import login_required
+from main.forms import ProviderForm
 
 from main.models import CustomerProfile, ProviderProfile
 
 from .forms import SignUpForm
-from django.contrib.auth import login
+from .tokens import account_activation_token
+
 
 # Create your views here.
+def activateEmail(request, user, to_email):
+    mail_subject = 'Activate your user account.'
+    message = render_to_string('accounts/template_activate_account.html', {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        'protocol': 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    if email.send():
+        messages.success(request, f'Dear <b>{user}</b>, please go to your email <b>{to_email}</b> inbox and click on \
+            received activation link to confirm and complete the registration. <b>Note:</b> Check your spam folder.')
+    else:
+        messages.error(request, f'Problem sending confirmation email to {to_email}, check if you typed it correctly.')
 
 
 def signup(request):
