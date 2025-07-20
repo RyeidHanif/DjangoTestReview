64983ee3b098792f49feff9a650efca63ce100e9
Ryeid Hanif <ryeidhanif@gmail.com>
2025-07-20 20:00:43 +0500

    feat: removed commented unrefactored views

--- customer/views.py
+++ customer/views.py
@@ -21,21 +21,6 @@ from .forms import AppointmentRecurrenceForm
 from django.contrib.auth import logout
 from django.views import View
 
-
-
-# @login_required(login_url="/login/")
-# def customerdashboard(request):
-#     if request.method == "POST":
-#         if request.POST.get("viewproviders"):
-#             return redirect("viewproviders")
-#         if request.POST.get("viewappointments"):
-#             return redirect("viewappointments")
-#         if request.POST.get("myprofile"):
-#             return redirect("userprofile")
-#         if request.POST.get("bookinghistory"):
-#             return redirect("bookinghistory")
-#     return render(request, "customer/customerdashboard.html")
-
 class CustomerDashboard(View , LoginRequiredMixin):
     login_url = "/login/"
     def get(self, request , *args , **kwargs):
@@ -53,30 +38,6 @@ class CustomerDashboard(View , LoginRequiredMixin):
         else :
             return self.get(request)
 customerdashboard = CustomerDashboard.as_view()
-
-
-
-# @login_required(login_url="/login/")
-# def viewproviders(request):
-#     query = request.GET.get('q')
-#     if query :
-#         providers = ProviderProfile.objects.filter(user__username__icontains= query).exclude(user=request.user)
-#     else:
-
-#         providers = ProviderProfile.objects.exclude(user=request.user)
-
-#     categories = ["doctor", "consultant", "therapist", "counsellor"]
-#     if request.method == "POST":
-#         messages.info(
-#             request, "You are being redirected to the service providers schedule"
-#         )
-#         return redirect("schedule", providerID=request.POST.get("bookappointment"))
-
-#     return render(
-#         request,
-#         "customer/viewproviders.html",
-#         {"providers": providers, "categories": categories},
-#     )
 
 
 class ViewProviders(ListView , LoginRequiredMixin):
@@ -110,48 +71,6 @@ viewproviders = ViewProviders.as_view()
 
 
 
-
-# @login_required(login_url="/login/")
-# def schedule(request, providerID):
-
-#     provider_profile = ProviderProfile.objects.get(id=providerID)
-#     provider = provider_profile.user
-#     slot_range = 1
-
-#     if request.method == "POST":
-#         if request.POST.get("week"):
-#             slot_range = 7
-#         elif request.POST.get("day"):
-#             slot_range = 1
-#         elif request.POST.get("slot_range"):
-#             slot_range = int(request.POST.get("slot_range"))
-        
-
-#         available_slots = get_available_slots(provider, slot_range)
-
-
-#         if request.POST.get("addappointment"):
-#             index = int(request.POST.get("addappointment"))
-#             timeslot = available_slots[index]
-#             request.session["timeslot_tuple"] = (
-#                 timeslot[0].isoformat(),
-#                 timeslot[1].isoformat(),
-#             )
-            
-#             return redirect("addappointment", providerUserID=provider.id)
-
-#     else:
-#         available_slots = get_available_slots(provider, slot_range)
-
-#     return render(
-#         request,
-#         "customer/schedule.html",
-#         {
-#             "available_slots": available_slots,
-#             "provider": provider,
-#             "slot_range": slot_range,
-#         },
-#     )
 
 
 class Schedule(View , LoginRequiredMixin):
@@ -204,150 +123,6 @@ schedule = Schedule.as_view()
 
 
 
-
-
-
-
-
-# @login_required(login_url="/login/")
-# def addappointment(request, providerUserID):
-
-#     mode = request.session.get("mode", "normal")
-#     print(f"DEBUG : THE CURRENT MODE IS {mode}")
-#     timeslot = request.session.get("timeslot_tuple", [])
-
-#     provider_user = User.objects.get(id=providerUserID)
-#     provider = ProviderProfile.objects.get(user=provider_user)
-#     customer = request.user
-
-#     start_datetime = datetime.fromisoformat(timeslot[0])
-#     end_datetime = datetime.fromisoformat(timeslot[1])
-#     total_price = calculate_total_price(provider)
-
-#     print(f"DEBUG : THE CURRENT MODE IS {mode}")
-#     if mode == "normal":
-#         print(f"DEBUG : THE CURRENT MODE IS {mode}")
-#         recurrence_form = AppointmentRecurrenceForm()
-
-#         if not check_appointment_exists(customer, provider_user):
-#             messages.warning(
-#                 request,
-#                 "You already have an appointment with this provider. Cancel that one first.",
-#             )
-#             return redirect("viewproviders")
-
-#         if request.method == "POST":
-#             print(f"DEBUG : THE CURRENT MODE IS {mode}")
-#             recurrence_form = AppointmentRecurrenceForm(request.POST)
-#             if request.POST.get("confirm"):
-#                 if recurrence_form.is_valid():
-#                     recurrence_frequency = recurrence_form.cleaned_data["recurrence"]
-#                     until_date = recurrence_form.cleaned_data["until_date"]
-#                 else:
-#                     recurrence_frequency = None
-#                     until_date = None
-
-#                 special_requests = request.POST.get("special_requests", "")
-
-#                 appointment = create_and_save_appointment(
-#                     customer,
-#                     provider_user,
-#                     start_datetime,
-#                     end_datetime,
-#                     total_price,
-#                     special_requests,
-#                     recurrence_frequency,
-#                     until_date,
-#                 )
-
-#                 # Removed google calendar API integration from here to prevent ghost appointments 
-#                 if appointment.provider.notification_settings.preferences == "all":
-#                     EmailPendingAppointment(
-#                         request,
-#                         customer,
-#                         provider_user,
-#                         start_datetime,
-#                         end_datetime,
-#                         provider_user.email,
-#                         special_requests,
-#                     )
-
-#                 messages.success(request, "Appointment created successfully")
-#                 return redirect("customerdashboard")
-
-#             elif request.POST.get("cancel"):
-#                 messages.info(request, "Appointment was not created")
-#                 return redirect("customerdashboard")
-
-#     elif mode == "reschedule":
-#         print(f"DEBUG : THE CURRENT MODE IS {mode}")
-#         appointment = Appointment.objects.filter(customer=customer, provider=provider_user).first()
-#         recurrence_form = AppointmentRecurrenceForm(initial={
-#             "recurrence": appointment.recurrence_frequency,
-#             "until_date": appointment.recurrence_until
-#         })
-        
-#         if not appointment:
-#             messages.error(request, "No existing appointment found for rescheduling.")
-#             return redirect("viewappointments")
-
-#         if request.method == "POST":
-#             print(f"DEBUG : THE CURRENT MODE IS {mode}")
-#             recurrence_form = AppointmentRecurrenceForm(request.POST)
-#             if recurrence_form.is_valid():
-#                 if recurrence_form.is_valid():
-#                     recurrence_frequency = recurrence_form.cleaned_data["recurrence"]
-#                     until_date = recurrence_form.cleaned_data["until_date"]
-#                 else:
-#                     recurrence_frequency = appointment.recurrence_frequency
-#                     until_date = appointment.recurrence_until
-
-#             if request.POST.get("confirm"):
-#                 old_start = appointment.date_start
-#                 old_end = appointment.date_end
-
-#                 appointment.date_start = start_datetime
-#                 appointment.date_end = end_datetime
-#                 appointment.status = "rescheduled"
-#                 appointment.recurrence_frequency = recurrence_frequency
-#                 appointment.recurrence_until = until_date
-#                 appointment.special_requests = request.POST.get("special_requests", "")
-#                 appointment.save()
-
-                
-#                 if appointment.provider.notification_settings.preferences == "all":
-
-#                     EmailRescheduledAppointment(
-#                         request,
-#                         customer,
-#                         provider_user,
-#                         localtime(old_start),
-#                         localtime(old_end),
-#                         localtime(appointment.date_start),
-#                         localtime(appointment.date_end),
-#                         provider_user.email,
-#                         appointment.special_requests,
-#                     )
-
-#                 request.session.pop("mode", None)
-#                 messages.success(request, "Appointment rescheduled successfully")
-#                 return redirect("viewappointments")
-
-#             elif request.POST.get("cancel"):
-#                 messages.info(request, "Reschedule cancelled")
-#                 return redirect("viewappointments")
-
-#     return render(
-#         request,
-#         "customer/addappointment.html",
-#         {
-#             "start": start_datetime,
-#             "end": end_datetime,
-#             "form": recurrence_form,
-#             "mode": mode,
-#             "appointment": appointment if mode == "reschedule" else None,
-#         },
-#     )
 
 
 
@@ -467,64 +242,6 @@ addappointment = AddAppointment.as_view()
 
 
 
-# @login_required(login_url="/login/")
-# def viewappointments(request):
-#     query =  request.GET.get("q")
-#     if query :
-#         myappointments = (
-#         Appointment.objects.filter(customer=request.user, provider__username__icontains=query)
-#         .order_by("-date_added")
-#         .all()
-#         .exclude(status="rejected")
-#         .exclude(status="cancelled").exclude(status="completed")
-#     )
-#     else:
-
-#         myappointments = (
-#             Appointment.objects.filter(customer=request.user)
-#             .order_by("-date_added")
-#             .all()
-#             .exclude(status="rejected")
-#             .exclude(status="cancelled").exclude(status="completed")
-#         )
-#     if request.method == "POST":
-#         if request.POST.get("reschedule"):
-#             messages.warning(
-#                 request,
-#                 "This will change the status to Rescheduled but the event for now will remain in the calendar  because the provider will have to review the timings again ",
-#             )
-#             change_appointment = Appointment.objects.get(
-#                 id=request.POST.get("reschedule")
-#             )
-#             if change_appointment.status != "accepted":
-#                 messages.error(
-#                     request, "sorry you cannot reschedule a non accepted appointment "
-#                 )
-#                 return redirect("viewappointments")
-#             else:
-#                 return redirect(
-#                     "reschedule", appointment_id=request.POST.get("reschedule")
-#                 )
-#         if request.POST.get("cancel"):
-#             appointment = Appointment.objects.get(id=request.POST.get("cancel"))
-#             count_cancel = cancellation(request.user , appointment)
-#             if appointment.status == "accepted":
-#                 service = get_calendar_service(appointment.provider)
-#                 service.events().delete(calendarId="primary" , eventId = appointment.event_id).execute()
-#             appointment.status = "cancelled"
-#             appointment.save()
-#             if count_cancel >= 3 :
-#                 request.user.is_active = False
-#                 request.user.save()
-#                 logout(request)
-#                 messages.warning(request,"You have cancelled too many appointments in a shot span , your account has been deactivated ")
-#                 return redirect("home")
-#             else:
-#                 messages.success(request, "Cancelled successfully ")
-#                 return redirect("viewappointments")
-#     return render(
-#         request, "customer/viewappointments.html", {"appointments": myappointments}
-#     )
 
 
 class ViewAppointments(View , LoginRequiredMixin):
--- provider/views.py
+++ provider/views.py
@@ -26,23 +26,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
-# @login_required(login_url="/login/")
-# def providerdashboard(request):
-#     if request.method == "POST":
-#         if request.POST.get("myprofile"):
-#             return redirect("userprofile")
-#         if request.POST.get("viewanalytics"):
-#             return redirect("viewanalytics")
-#         if request.POST.get("viewmyappointments"):
-#             return redirect("view_my_appointments")
-#         if request.POST.get("viewpendingappointments"):
-#             return redirect("view_pending_appointments")
-#         if request.POST.get("myavailability"):
-#             return redirect("myavailability")
-
-#     return render(request, "provider/providerdashboard.html")
-
-
 class ProviderDashboard(View , LoginRequiredMixin):
     login_url = "/login/"
     def get(self , request , *args , **kwargs):
@@ -64,62 +47,6 @@ providerdashboard = ProviderDashboard.as_view()
 
 
 
-
-
-
-# @login_required(login_url="/login/")
-# def view_my_appointments(request):
-#     query = request.GET.get("q")
-#     if query:
-#         my_appointments = Appointment.objects.filter(
-#         provider=request.user, status="accepted", customer__username__icontains=query,
-#         ).order_by("-date_added")
-#     else : 
-#         my_appointments = Appointment.objects.filter(
-#         provider=request.user, status="accepted"
-#         ).order_by("-date_added")
-#     if request.method == "POST":
-#         if request.POST.get("cancel"):
-#             cancel_appointment = Appointment.objects.get(id=request.POST.get("cancel"))
-#             to_email = cancel_appointment.customer.email
-#             customer = cancel_appointment.customer
-#             provider = cancel_appointment.provider
-#             cancel_appointment.status = "cancelled"
-#             service = get_calendar_service(request.user)
-#             service.events().delete(
-#                 calendarId="primary", eventId=cancel_appointment.event_id
-#             ).execute()
-#             cancel_appointment.save()
-#             if customer.notification_settings.preferences == "all":
-#                 EmailCancelledAppointment(request, customer, provider, to_email)
-#             count_cancel = cancellation(request.user , cancel_appointment)
-#             if count_cancel >= 3 :
-#                 request.user.is_active = False
-#                 request.user.save()
-#                 logout(request)
-#                 messages.warning(request , "you cancelled too many apointments after deadline in a short span of time ")
-#                 return redirect("home")
-#             else:
-#                 return redirect("view_my_appointments")
-
-#         if request.POST.get("markcompleted"):
-#             appointment = Appointment.objects.get(id=request.POST.get("markcompleted"))
-#             current_datetime = now()
-#             if appointment.date_start > current_datetime:
-#                 messages.warning(
-#                     request,
-#                     "This appointment has not happened yet . you cannot mark it complete",
-#                 )
-#                 return redirect("view_my_appointments")
-#             else:
-#                 appointment.status = "completed"
-#                 appointment.save()
-#                 messages.success(request, "marked successfully")
-#                 return redirect("view_my_appointments")
-
-#     return render(
-#         request, "provider/view_my_appointments.html", {"appointments": my_appointments}
-#     )
 
 
 class ViewMyAppointments(View, LoginRequiredMixin):
@@ -182,92 +109,6 @@ view_my_appointments = ViewMyAppointments.as_view()
 
 
 
-
-
-
-# @login_required(login_url="/login/")
-# def view_pending_appointments(request):
-
-#     query= request.GET.get("q")
-#     if query :
-#         my_appointments = Appointment.objects.filter(status__in=["pending", "rescheduled"] , provider = request.user , customer__username__icontains=query)
-#     else :
-#         my_appointments = Appointment.objects.filter(status__in=["pending", "rescheduled"] , provider = request.user)
-
-    
-#     if request.method == "POST":
-#         if request.POST.get("reject"):
-#             appointment = Appointment.objects.get(id=request.POST.get("reject"))
-#             service = get_calendar_service(request.user)
-#             service.events().delete(calendarId="primary", eventId=appointment.event_id).execute()
-#             if appointment.status == "pending":
-#                 appointment.status = "rejected"
-#                 if appointment.customer.notification_settings.preferences == "all":
-#                     EmailDeclinedAppointment(
-#                         request,
-#                         appointment.customer,
-#                         appointment.provider,
-#                         "N/A",
-#                         to_email=appointment.customer.email,
-#                     )
-#                 appointment.save()
-#                 messages.success(request, " appointment rejected successfully")
-#                 return redirect("view_pending_appointments")
-#             elif appointment.status == "rescheduled":
-#                 appointment.status ="cancelled"
-#                 appointment.save()
-#                 messages.info(request , "reschedule rejected successfully ")
-#                 if appointment.customer.notification_settings.preferences == "all":
-#                     EmailRescheduleDeclined(request , appointment.customer,  appointment.provider , appointment.date_start , appointment.date_end , appointment.customer.email)
-#         if request.POST.get("accept") :
-#             appointment = Appointment.objects.get(id=request.POST.get("accept"))
-#             if appointment.status == "pending":
-#                 appointment.status = "accepted"
-#                 #add the rest here 
-#                 summary = f"Appointment with {appointment.customer.username}"
-#                 service = get_calendar_service(appointment.provider)
-        
-#                 timeslot = (localtime(appointment.date_start).isoformat() , localtime(appointment.date_end).isoformat())
-#                 event = create_google_calendar_event(
-#                     service,
-#                     timeslot,
-#                     summary,
-#                     appointment.customer.email,
-#                     appointment.recurrence_frequency,
-#                     appointment.recurrence_until,
-#                     appointment,
-#                 )
-
-#                 appointment.event_id = event["id"]
-#                 appointment.save()
-#                 if appointment.customer.notification_settings.preferences == "all":
-#                     EmailConfirmedAppointment(
-#                         request,
-#                         appointment.customer,
-#                         appointment.provider,
-#                         localtime(appointment.date_start),
-#                         localtime(appointment.date_end),
-#                         to_email=appointment.customer.email,
-#                     )
-#                 messages.success(request, "Accepted successflly ")
-#                 return redirect("view_pending_appointments")
-#             elif appointment.status == "rescheduled":
-#                 appointment.status = "accepted"
-#                 service = get_calendar_service(request.user)
-#                 reschedule_google_event(
-#                     service, appointment.event_id, localtime(appointment.date_start).isoformat(), localtime(appointment.date_end).isoformat() , appointment.recurrence_frequency , appointment.recurrence_until
-#                 )
-#                 SendEmailRescheduleAccepted(request, appointment.customer , appointment.provider , appointment.date_start ,appointment.date_end, appointment.customer.email)
-#                 appointment.save()
-
-
-
-
-#     return render(
-#         request,
-#         "provider/view_pending_appointments.html",
-#         {"appointments": my_appointments},
-#     )
 
 
 class ViewPendingAppointments(View , LoginRequiredMixin):
@@ -366,48 +207,6 @@ class ViewPendingAppointments(View , LoginRequiredMixin):
 view_pending_appointments = ViewPendingAppointments.as_view()
 
 
-# def myavailability(request):
-#     if request.method == "POST":
-#         form = AvailabilityForm(request.POST)
-#         if form.is_valid():
-#             start_date = form.cleaned_data["start_date"]
-#             end_date = form.cleaned_data["end_date"]
-#             start_time = form.cleaned_data["start_time"]
-#             end_time = form.cleaned_data["end_time"]
-#             cause = form.cleaned_data["cause"]
-#             start_datetime = make_aware(
-#                 datetime.combine(start_date, start_time), timezone=get_current_timezone()
-#             )
-#             end_datetime = make_aware(datetime.combine(end_date, end_time), timezone=get_current_timezone())
-#             start_datetime_iso = start_datetime.isoformat()
-#             end_datetime_iso = end_datetime.isoformat()
-#             service = get_calendar_service(request.user)
-#             event = {
-#                 "summary": "Unavailable",
-#                 "location": "NA",
-#                 "description": cause,
-#                 "start": {
-#                     "dateTime": start_datetime_iso,
-#                     "timeZone": "Asia/Karachi",
-#                 },
-#                 "end": {
-#                     "dateTime": end_datetime_iso,
-#                     "timeZone": "Asia/Karachi",
-#                 },
-#                 "reminders": {
-#                     "useDefault": True,
-#                 },
-#             }
-#             service.events().insert(calendarId="primary", body=event).execute()
-#             messages.success(request, "Added time block successfully")
-#             return redirect("myavailability")
-
-#     else:
-#         form = AvailabilityForm()
-
-#     return render(request, "provider/myavailability.html", {"form": form})
-
-
 class MyAvailability(View , LoginRequiredMixin):
     def get(self , request , *args , **kwargs):
         self.form = AvailabilityForm()
@@ -454,43 +253,6 @@ myavailability = MyAvailability.as_view()
 
 
 
-
-# @login_required(login_url="/login/")
-# def viewanalytics(request):
-#     revenue = 0
-#     myappointments = Appointment.objects.filter(provider=request.user)
-#     total_statuses = 0
-#     percentage_statuses_dict = {}
-#     statuses = {"pending": 0,
-#                 "accepted":0 ,
-#                 "rejected": 0,
-#                 "cancelled": 0,
-#                 "completed": 0,
-#                 "rescheduled":0,
-#                 }
-#     customers = []
-#     for appointment in myappointments :
-#         statuses[appointment.status] +=1
-#         total_statuses +=1
-#         customers.append(appointment.customer.username)
-#         if appointment.status in ["accepted" , "completed"]:
-#             revenue += appointment.total_price
-    
-#     admin_cut = 0.05 * revenue
-#     for key,value in statuses.items():
-#             if total_statuses != 0 :
-#                 percentage = (value/total_statuses)*100
-#             else :
-#                 percentage= 0
-#             percentage_statuses_dict[key] = percentage
-
-
-
-    
-
-    
-#     return render(request , "provider/viewanalytics.html" , {"customers": customers , "myappointments": myappointments , "statuses": statuses , "revenue": revenue,  "admin_cut": admin_cut, "percentage_statuses_dict":percentage_statuses_dict} )
-        
 
 
 class ViewAnalytics(View, LoginRequiredMixin):
